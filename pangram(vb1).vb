a9c5ca40a13dfde44cde13a20651093a9a328103
MishgaAzeem <94231425+MishgaAzeem@users.noreply.github.com>
2021-11-17 00:28:59 +0500

    Add files via upload

---
+++ pangram(vb1).vb
@@ -0,0 +1,36 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim Str1 As String
+        Dim Alpha(25) As Char
+        Dim ThisAlph As Char
+        Dim AlphaPos As Integer
+        Dim isPangram = True
+
+        Str1 = ""
+        Alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        ThisAlph = ""
+        AlphaPos = 0
+
+        Console.Write("Enter the string: ")
+        Str1 = UCase(Console.ReadLine)
+
+        For Index = 1 To Len(Alpha)
+            ThisAlph = Mid(Alpha, Index, 1)
+            AlphaPos = InStr(Str1, ThisAlph)
+            If AlphaPos = 0 Then
+                isPangram = False
+                Exit For
+            End If
+        Next
+
+        If isPangram = True Then
+            Console.WriteLine("This string has all the alphabets.")
+        Else
+            Console.WriteLine("This string does not have all the alphabets.")
+        End If
+
+        Console.ReadKey()
+    End Sub
+
+End Module
---
+++ replacing characters (vb2).vb
@@ -0,0 +1,35 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim Str1, Str2 As String
+        Dim Char1, Char2, ThisChar As Char
+        Dim Count As Integer
+
+        Str1 = ""
+        Str2 = ""
+        Char1 = ""
+        Char2 = ""
+        ThisChar = ""
+        Count = 0
+
+        Console.Write("Enter String to process: ")
+        Str1 = UCase(Console.ReadLine)
+        Console.Write("Enter character to be replaced: ", Char1)
+        Char1 = UCase(Console.ReadLine)
+        Console.Write("Enter new character: ", Char2)
+        Char2 = UCase(Console.ReadLine)
+
+        For Count = 1 To Len(Str1)
+            ThisChar = Mid(Str1, Count, 1)
+            If ThisChar = Char1 Then 
+                ThisChar = Char2
+            End If
+            Str2 = Str2 & ThisChar
+        Next
+
+        Console.WriteLine("This is the new string: " & Str2)
+
+        Console.ReadKey()
+    End Sub
+
+End Module
